5eba7bb2ea00ee556344f26dfe333c0a71d3f19d
wy38 <wy38@duke.edu>
2018-03-05 20:26:49 -0500

    Adds comment to initial version

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ var inputContent;
 
 function doTransformation() {
     inputContent = document.getElementsByName('inputText')[0].value;
+    // Checks if the content in the input textbox is empty. If so, return an error message.
     if (inputContent == "") {
         alert("Please fill in valid URL. ");
         return;
     }
     alert("SEE! Magic! ");
+    // Does transformation on input text.
     document.getElementsByName('outputText')[0].value = inputContent + "!!!";
 }
