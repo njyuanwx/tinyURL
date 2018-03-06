922eb489402c8f594039c893900f9e5321dfbe3f
wy38 <wy38@duke.edu>
2018-03-06 15:32:08 -0500

    Updates front-end design

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
 var inputContent;
+var outputContent;
 
 function doTransformation() {
     inputContent = document.getElementsByName('inputText')[0].value;
     // Checks if the content in the input textbox is empty. If so, return an error message.
     if (inputContent == "") {
-        alert("Please fill in valid URL. ");
+        alert("Please fill in a valid URL. ");
         return;
     }
-    alert("SEE! Magic! ");
-    // Does transformation on input text.
-    document.getElementsByName('outputText')[0].value = inputContent + "!!!";
+
+    // Does transformation on input text. (TODO)
+    outputContent = inputContent + "!!!";
+
+    // Prints the output URL.
+    document.getElementsByName('outputText')[0].value = outputContent;
+    alert("Your shortened URL is generated.");
+}
+
+function doCustomization() {
+    inputContent = document.getElementsByName('inputText')[0].value;
+    // Checks if the content in the input textbox is empty. If so, return an error message.
+    if (inputContent == "") {
+        alert("Please fill in a valid URL. ");
+        return;
+    }
+
+    outputContent = document.getElementsByName('outputText')[0].value;
+    // Checks if the content in the output textbox is empty. If so, return an error message.
+    if (outputContent == "") {
+        alert("Please fill in a valid customized URL. ");
+        return;
+    }
+    // Checks whether this customized URL has been used. (TODO)
+
+    // Prints the output URL.
+    document.getElementsByName('outputText')[0].value = outputContent;
+    alert("Your customized URL is confirmed.")
 }
