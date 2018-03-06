57508d02dbc4a9d08474ee190e9f34c3d16d1d76
wy38 <wy38@duke.edu>
2018-03-06 16:27:20 -0500

    Add table indicate top 5 clicked shortened URLs

--- main.js
+++ main.js
@@ -37,3 +37,20 @@ function doCustomization() {
     document.getElementsByName('outputText')[0].value = outputContent;
     alert("Your customized URL is confirmed.")
 }
+
+function updateTopClickTable() {
+    // Finds top 5 clicked URL. (TODO)
+
+    // Updates the content in the table. (TODO)
+    // Now just hard coded.
+    document.getElementById("topClickTable").rows[1].cells[0].innerHTML = "https://shortenedURL.com/test1";
+    document.getElementById("topClickTable").rows[1].cells[1].innerHTML = "10000";
+    document.getElementById("topClickTable").rows[2].cells[0].innerHTML = "https://shortenedURL.com/test2";
+    document.getElementById("topClickTable").rows[2].cells[1].innerHTML = "1000";
+    document.getElementById("topClickTable").rows[3].cells[0].innerHTML = "https://shortenedURL.com/test3";
+    document.getElementById("topClickTable").rows[3].cells[1].innerHTML = "100";
+    document.getElementById("topClickTable").rows[4].cells[0].innerHTML = "https://shortenedURL.com/test4";
+    document.getElementById("topClickTable").rows[4].cells[1].innerHTML = "10";
+    document.getElementById("topClickTable").rows[5].cells[0].innerHTML = "https://shortenedURL.com/test5";
+    document.getElementById("topClickTable").rows[5].cells[1].innerHTML = "1";
+}
